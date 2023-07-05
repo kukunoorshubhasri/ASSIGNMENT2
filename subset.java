c1c35c3f11ec2ab57dcc70da7d742487abf04958
kukunoorshubhasri <128603291+kukunoorshubhasri@users.noreply.github.com>
2023-07-05 05:53:05 -0700

    subset

---
+++ subset.java
@@ -0,0 +1,48 @@
+//WAP to check whether an array is a subset of another array.
+
+package assignment;
+import java.util.Scanner;
+public class subset
+{
+	public static void main(String[] args)
+	   {
+		   int n,m; 
+		   Scanner sc=new Scanner(System.in);   
+		   System.out.println("Enter Main Array size:");
+		   n=sc.nextInt();
+		   System.out.println("Enter size of array to check");
+		   m=sc.nextInt();
+		   int a[]=new int[n];
+		   int b[]=new int[m];
+		   System.out.println("Enter main array elements:");
+		   for(int i=0;i<n;i++)
+		     a[i]=sc.nextInt();
+		   int j=0;
+		   System.out.println("Enter array to check");
+		   for(j=0;j<m;j++)
+		      b[j]=sc.nextInt();
+		   int x=0; 
+		   for(int i=0;i<m;i++)
+		    {
+		      for(j=0;j<n;j++)
+		      {
+		        if(b[i]==a[j])
+		        {
+		         break;
+		        }
+		      }
+		      if(j==n)
+		      {
+		      x=1;
+		      System.out.println("Given array is not a subset");
+		       break;
+		      }
+		    } 
+		  if(x==0)
+		  {
+		    System.out.println("Given array is a subset of the main array");
+		  }  
+		 }
+		}
+	   
+	     
